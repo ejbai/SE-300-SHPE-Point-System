7be78c25fa821c3b3f76a81ca641a94ca9f8fe2e
ejbai <111667032+ejbai@users.noreply.github.com>
2026-04-01 15:27:29 -0400

    Remove sample events from create_tables.sql
    Removed sample events insertion from create_tables.sql.

--- create_tables.sql
+++ create_tables.sql
@@ -31,14 +31,3 @@ CREATE TABLE accounts (
 	userRank ENUM ('Member', 'Director', 'Chairman') NOT NULL,
 	PRIMARY KEY (username)
 );
-
---------------------------------------------------------------------------
-
--- Create some sample events
-
-INSERT INTO events VALUES
-	('Event1', 'The park', '2026-03-04', 20, 5),
-	('Event2', 'The lake', '2026-03-06', 12, 3),
-	('Event3', 'The courtyard', '2026-03-07', 10, 7)
-;
-	
